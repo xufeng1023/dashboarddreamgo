143249fb0ae35cdb58eb00ccc125f881c07272f0
xufeng1023 <xufeng1023@hotmail.com>
2019-05-17 10:11:21 -0400

    'fix-login'

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <style>
 div.other{
-    max-width: 20rem;
+    max-width: 17rem;
 }
 .nowrap{
     white-space: nowrap;
